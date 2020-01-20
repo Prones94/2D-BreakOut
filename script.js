11aeba9024f8bf7c018bbe11069527da942dc7ca
Ian Rones <ronesp.85@gmail.com>
2020-01-20 12:32:54 -0800

    [add] drawBall and draw functions

---
+++ script.js
@@ -0,0 +1,26 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+
+// draw Ball
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// draw function with interval timer of 10 seconds
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall()
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 50)
+
+
