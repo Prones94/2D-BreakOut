f581449a20b07575d0aa63f3fdba7b0b263eae5b
Ian Rones <ronesp.85@gmail.com>
2020-01-20 12:55:41 -0800

    [add] code to allow ball to bounce off sides of canvas within draw function

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Variable Declarations
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
+
 
 // draw Ball
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
@@ -20,7 +23,13 @@ function draw() {
     drawBall()
     x += dx;
     y += dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx
+    }
+    if (y + dy >canvas.height- ballRadius || y + dy < ballRadius){
+        dy = -dy
+    }
 }
-setInterval(draw, 50)
+setInterval(draw, 10)
 
 
