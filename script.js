f1947078aa541c102be1d410852273e170a33361
Ian Rones <ronesp.85@gmail.com>
2020-01-20 15:41:33 -0800

    [add] draw paddle function with working javascript

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 let ballRadius = 10;
-
+let paddleWidth = 75;
+let paddleHeight = 10;
+let paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
 // draw Ball
 function drawBall(){
@@ -17,10 +21,21 @@ function drawBall(){
     ctx.closePath();
 }
 
+// Draw Paddle
+function drawPaddle(){
+    ctx.beginPath();
+    
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
 // draw function with interval timer of 10 seconds
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall()
+    drawPaddle()
     x += dx;
     y += dy;
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -29,7 +44,40 @@ function draw() {
     if (y + dy >canvas.height- ballRadius || y + dy < ballRadius){
         dy = -dy
     }
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    } else if(leftPressed) {
+        paddleX -= 7;
+        if(paddleX < 0){
+            paddleX = 0
+        }
+    }
 }
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10)
 
 
