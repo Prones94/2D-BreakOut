8611ad6ff6d98e93e0521c17cc7cd0ff1b26e4c0
Ian Rones <ronesp.85@gmail.com>
2020-01-21 10:20:24 -0800

    [add] drawScore function with win alert

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Variable Declarations
-let canvas = document.getElementById('myCanvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
@@ -18,12 +18,13 @@ let brickHeight = 20;
 let brickPadding = 10;
 let brickOffsetTop = 30;
 let brickOffsetLeft = 30;
+let score = 0;
 
 var bricks = [];
 for(var c = 0; c < brickColumnCount; c++){
     bricks[c] = [];
-    for(var r= 0; r< brickColumnCount; r++){
-        bricks[c][r] = {x:0, y:0};
+    for(var r= 0; r < brickColumnCount; r++){
+        bricks[c][r] = {x:0, y:0, status: 1};
     }
 };
 
@@ -49,6 +50,7 @@ function drawPaddle(){
 function drawBricks(){
     for(var c = 0; c < brickColumnCount; c++){
         for (var r= 0; r< brickRowCount; r++){
+            if(bricks[c][r].status == 1){
             var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
             var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
             bricks[c][r].x = brickX;
@@ -58,16 +60,26 @@ function drawBricks(){
             ctx.fillStyle = '#0095DD';
             ctx.fill();
             ctx.closePath();
+            }
         }
     }
 }
+
+// draw Score Board
+function drawScore(){
+    ctx.font = '16px Arial';
+    ctx.fillStyle = '#0095DD';
+    ctx.fillText('Score: ' + score, 8, 20);
+};
 
 // draw function with interval timer of 10 seconds
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBricks();
-    drawBall()
-    drawPaddle()
+    drawBall();
+    drawPaddle();
+    drawScore();
+    collisionDetection();
     x += dx;
     y += dy;
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -113,6 +125,22 @@ function keyUpHandler(e) {
     }
     else if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
+    }
+}
+
+// Collision Detection with Bricks & Ball
+function collisionDetection(){
+    for(var c = 0; c < brickColumnCount; c++){
+        for(var r = 0; r < brickRowCount; r++){
+            var b = bricks[c][r];
+            if(b.status == 1){
+                if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                }
+            }
+        }
     }
 }
 
